df49389880871d00510e6f4f5f705f8e1bf8c4e3
luanhuynh <luanhhuynh7181@gmail.com>
2025-08-31 23:27:04 +0700

    first commit

---
+++ assets/scripts/utils/Transition.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Transitsion')
+export class Transitsion extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
