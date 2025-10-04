adf4a595b9b0a6207554e094b64a83c29ac15755
luanhuynh <luanhhuynh7181@gmail.com>
2025-10-04 21:20:04 +0700

    add ui tutorial

--- assets/scripts/components/main_menu/popup/PopupTutorial.ts
+++ assets/scripts/components/main_menu/popup/PopupTutorial.ts
@@ -1,14 +1,37 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Button, Component, Node, PageView } from 'cc';
 const { ccclass, property } = _decorator;
 
-@ccclass('PopupLeaderBoard')
-export class PopupLeaderBoard extends Component {
-    start() {
+@ccclass('PopupTutorial')
+export class PopupTutorial extends Component {
 
+    @property(PageView)
+    pageView: PageView = null!;
+
+    @property(Button)
+    btnNext: Button = null!;
+
+    @property(Button)
+    btnPrev: Button = null!;
+
+    onLoad() {
+        this.pageView.node.on('page-turning', this.onPageTurned, this);
+        this.btnPrev.node.active = false;
     }
 
-    update(deltaTime: number) {
-        
+    onPageTurned(pageView: PageView) {
+        const index = this.pageView.getCurrentPageIndex();
+        const total = this.pageView.getPages().length;
+        this.btnPrev.node.active = index > 0;
+        this.btnNext.node.active = index < total - 1;
     }
+
+    onClickNext() {
+        this.pageView.scrollToPage(this.pageView.getCurrentPageIndex() + 1);
+    }
+    onClickPrev() {
+        this.pageView.scrollToPage(this.pageView.getCurrentPageIndex() - 1);
+    }
+
 }
-
+
+
